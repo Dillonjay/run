89bed3a2fbc3a897ad70d8d4146c84da7bab723a
Dillon Lundell <dillonjaylundell@gmail.com>
2017-02-18 17:48:43 -0600

    seperate server logic in to a config file

---
+++ server/config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var bodyParser = require('body-parser');
+var request = require('request');
+var yelp = require('./yelp.js').yelp;
+
+module.exports = function (app, express) {
+
+  // Statically serve the entire client folder.
+  app.use(express.static(path.join(__dirname + '/../client')));
+
+  app.use(bodyParser.json());
+
+  // Initially send the index.html file. 
+  app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+  });
+
+  app.post('/search', function(req,res) {
+    console.log('req', req.body);
+    console.log(yelp( {location: req.body.city} ))
+    return request(yelp({location: req.body.city}), function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send(body).status(200) 
+      } else {
+        console.log(body)
+        console.log('failed')
+        res.sendStatus(400)
+      };
+    });
+  });
+};
--- server/server.js
+++ server/server.js
@@ -1,31 +1,8 @@
 var express = require('express');	
-var path = require('path');
-var bodyParser = require('body-parser');
-var request = require('request');
-var yelp = require('./yelp.js').yelp;
 
 var app = express();
-// Statically serve the entire client folder.
-app.use(express.static(__dirname + '/../client'));
+
+require('./config.js')(app, express);
 
 
-app.use(bodyParser.json())
-// Initially send the index.html file. 
-app.get('/', function(req, res){
- 	res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
-})
-
-app.post('/search', function(req,res){
-	console.log('req', req.body);
-	return request(yelp(), function(error, response, body){
-  		if (!error && response.statusCode == 200) {
-    	res.send(body).status(200) 
-  		} else {
-  			console.log('failed')
-  		}
-  	})
-})
-
-app.listen(3000, function(){
-	console.log('listening at localhost:3000')	
-});
+app.listen(process.env.PORT || 3000);
