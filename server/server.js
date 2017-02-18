25075de15d5c5cdc6de8360883fb7851ff8666dd
Dillon Lundell <dillonjaylundell@gmail.com>
2017-02-18 17:27:42 -0600

    create yelp function for hitting yelp search api. Send back data to client

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');	
 var path = require('path');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var request = require('request');
+var yelp = require('./yelp.js').yelp;
 
 var app = express();
 // Statically serve the entire client folder.
@@ -10,12 +12,18 @@ app.use(express.static(__dirname + '/../client'));
 app.use(bodyParser.json())
 // Initially send the index.html file. 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+ 	res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
 })
 
 app.post('/search', function(req,res){
 	console.log('req', req.body);
-	res.sendStatus(200)
+	return request(yelp(), function(error, response, body){
+  		if (!error && response.statusCode == 200) {
+    	res.send(body).status(200) 
+  		} else {
+  			console.log('failed')
+  		}
+  	})
 })
 
 app.listen(3000, function(){
---
+++ server/yelp.js
@@ -0,0 +1,39 @@
+var request = require('request');
+var oauthSignature = require('oauth-signature');  
+var n = require('nonce')();  
+var qs = require('querystring');  
+
+exports.yelp = function(location) {
+  // The type of request will be a GET
+  var httpMethod = 'GET';
+  // The url used 
+  var url = 'http://api.yelp.com/v2/search';
+  // Set search term to coffee.
+  var defaultParameters = {
+ 	  term: 'Coffee'
+  };
+  // Required oauth paramaters. 
+  var requiredParameters = {
+    oauth_consumer_key : process.env.oath_consumer_key,
+    oauth_token : process.env.oauth_token,
+    oauth_nonce : n(),
+    oauth_timestamp : n().toString().substr(0,10),
+    oauth_signature_method : 'HMAC-SHA1',
+    oauth_version : '1.0'
+  };
+  // We combine all the parameters in order of importance 
+  var parameters = Object.assign(defaultParameters, location , requiredParameters);
+  // Set our secrets here
+  var consumerSecret = process.env.consumerSecret;
+  var tokenSecret = process.env.tokenSecret;
+  // Then we call Yelp's Oauth 1.0a server and get a signature.
+  var signature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
+
+  // We add the signature to the list of paramters 
+  parameters.oauth_signature = signature;
+
+  // Turn the paramters object, to a query string .
+  var paramURL = qs.stringify(parameters);
+  // Add paramUrl to url string.
+  return `${url}?${paramURL}`;  
+};
